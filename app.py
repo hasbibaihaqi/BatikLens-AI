5123652b9c171e7cf3f25086b15312d7696167a9
hasbibaihaqi <hasbipratama070@gmail.com>
2026-07-19 17:14:15 +0700

    Fix: Memaksa Legacy Keras untuk load model vgg16

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import os
+# BARIS PENYELAMAT: Harus dipanggil paling awal sebelum TensorFlow!
+os.environ['TF_USE_LEGACY_KERAS'] = '1'
+
 import json
 import uuid
 import numpy as np
@@ -7,8 +10,6 @@ from werkzeug.utils import secure_filename
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.vgg16 import preprocess_input
-import os
-os.environ['TF_USE_LEGACY_KERAS'] = '1'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads/'
