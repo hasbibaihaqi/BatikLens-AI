8a704077036c4bd84795c9ad47f101816a341f4d
hasbibaihaqi <hasbipratama070@gmail.com>
2026-07-19 15:35:29 +0700

    Fix Keras loading issue dengan TF_USE_LEGACY_KERAS

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from werkzeug.utils import secure_filename
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.vgg16 import preprocess_input
+import os
+os.environ['TF_USE_LEGACY_KERAS'] = '1'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads/'
